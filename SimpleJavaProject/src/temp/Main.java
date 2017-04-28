a5f7058772730d56cf3e474456d7d38631118edd
skryzhya <skrudge@yahoo.com>
2017-04-28 00:03:46 -0400

    Initial checkin to GiHub

---
+++ SimpleJavaProject/src/temp/Main.java
@@ -0,0 +1,9 @@
+package temp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Dummy message! ");
+	}
+
+}
---
+++ SimpleJavaProject/src/temp/Main2.java
@@ -0,0 +1,7 @@
+package temp;
+
+public class Main2 {
+	public static void main(String[] args) {
+		System.out.println("Dummy message 2! ");
+	}
+}
